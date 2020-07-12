90e01c4bd7e966d6e01711f09dec7656c5111309
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-13 00:08:43 +0530

    Activity hosts FrameLayout which changes fragments

--- app/src/main/java/com/developersbreach/searchinterface/MainActivity.kt
+++ app/src/main/java/com/developersbreach/searchinterface/MainActivity.kt
@@ -2,10 +2,20 @@ package com.developersbreach.searchinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.developersbreach.searchinterface.list.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = SearchFragment()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
