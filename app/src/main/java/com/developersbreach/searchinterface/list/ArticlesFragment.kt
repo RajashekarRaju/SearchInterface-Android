edf2817bf3f252cafd2c9bab238c0f3b673dff20
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-16 23:55:03 +0530

    New fragment transaction for SearchFragment on clicking the fab icon

--- app/src/main/java/com/developersbreach/searchinterface/list/ArticlesFragment.kt
+++ app/src/main/java/com/developersbreach/searchinterface/list/ArticlesFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.developersbreach.searchinterface.R
+import com.developersbreach.searchinterface.search.SearchFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ArticlesFragment : Fragment() {
 
@@ -26,6 +30,14 @@ class ArticlesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = view.findViewById(R.id.recycler_view)
+
+        view.findViewById<FloatingActionButton>(R.id.search_fab).setOnClickListener {
+            val fragment = SearchFragment()
+            val fragmentManager: FragmentManager = parentFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
